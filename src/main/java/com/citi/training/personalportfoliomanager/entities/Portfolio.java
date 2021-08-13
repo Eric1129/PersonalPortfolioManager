55589a2ba85643e98a74f64ef3b588a1784c8ccc
Tingyu Shen <tingyu.shen@mail.mcgill.ca>
2021-08-13 11:58:35 -0400

    entity

---
+++ src/main/java/com/citi/training/personalportfoliomanager/entities/Portfolio.java
@@ -0,0 +1,24 @@
+package com.citi.training.personalportfoliomanager.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name="portfolio")
+public class Portfolio implements Serializable {
+
+    @Id
+    @Column(name = "account_number", nullable = false)
+    private Integer accountNumber;
+
+    public Integer getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(Integer accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+}
