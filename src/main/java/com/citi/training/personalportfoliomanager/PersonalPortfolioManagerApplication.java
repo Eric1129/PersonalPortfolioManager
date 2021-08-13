d9e81aa6d86f7af0cd84bae8f74c0f80cce6971f
Tingyu Shen <tingyu.shen@mail.mcgill.ca>
2021-08-13 09:39:36 -0400

    setup

---
+++ src/main/java/com/citi/training/personalportfoliomanager/PersonalPortfolioManagerApplication.java
@@ -0,0 +1,13 @@
+package com.citi.training.personalportfoliomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalPortfolioManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalPortfolioManagerApplication.class, args);
+    }
+
+}
