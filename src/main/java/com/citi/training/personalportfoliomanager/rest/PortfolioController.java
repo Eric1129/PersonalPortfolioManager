8c8eb585966540f0d7098f8db70e56b066b7a58c
Abdul <arules14@gmail.com>
2021-08-13 18:29:48 +0000

    Added more methods to the controller class

--- src/main/java/com/citi/training/personalportfoliomanager/rest/PortfolioController.java
+++ src/main/java/com/citi/training/personalportfoliomanager/rest/PortfolioController.java
@@ -2,7 +2,7 @@ package com.citi.training.personalportfoliomanager.rest;
 
 
 import com.citi.training.personalportfoliomanager.entities.Portfolio;
-import com.citi.training.personalportfoliomanager.service.PortfolioServices;
+import com.citi.training.personalportfoliomanager.service.PortfolioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 public class PortfolioController {
 
     @Autowired
-    private PortfolioServices portfolioService;
+    private PortfolioService portfolioService;
 
     /**
      * Gets the account with id account_id
@@ -21,24 +21,67 @@ public class PortfolioController {
         return portfolioService.get(account_id);
     }
 
+    /**
+     * Gets the Net worth of all accounts
+     * @return (int) the net worth of all the accounts in this portfolio
+     */
     @RequestMapping(method = RequestMethod.GET, value="/networth")
-    public Portfolio getNetWorth(){
+    public int getNetWorth(){
         return portfolioService.getNetWorth();
     }
 
+    /**
+     * Gets the Cash value of all accounts
+     * @return (int) the cash value of all the accounts in this portfolio
+     */
     @RequestMapping(method = RequestMethod.GET, value="/cashvalue")
-    public Portfolio getNetWorth(){
+    public Portfolio getCashValue(){
         return portfolioService.getCashValue();
     }
 
+    /**
+     * Gets the Investment value of all accounts
+     * @return (int) the investment value of all the accounts in this portfolio
+     */
     @RequestMapping(method = RequestMethod.GET, value="/investmentvalue")
-    public Portfolio getNetWorth(){
+    public Portfolio getInvestmentValue(){
         return portfolioService.getInvestmentValue();
     }
 
+    /**
+     * Adds a new account to the portfolio table
+     * @param account
+     */
     @RequestMapping(method = RequestMethod.POST)
-    public void addAccount(@RequestBody CompactDisc disc) {
-        service.addNewCompactDisc(disc);
+    public void addAccount(@RequestBody Portfolio account) {
+        portfolioService.addNewAccount(account);
+    }
+
+    /**
+     * Delets an existing account from the portfolio table
+     * @param id
+     */
+    @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
+    public void deleteAccount(@PathVariable("id") int id) {
+        portfolioService.deleteAccount(id);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    public void deleteAccount(@RequestBody Portfolio account) {
+        portfolioService.deleteAccount(account);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/add/{id}/{value}")
+    public void deposit(@PathVariable("id") int id, @PathVariable("value") double value){
+        //need to add checks to ensure users aren't depositing negative values
+        portfolioService.deposit(id, value);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/add/{id}/{value}")
+    public void withdraw(@PathVariable("id") int id, @PathVariable("value") double value){
+        //need to add checks to ensure users aren't withdrawing negative values
+        //need to ensure users are not withdrawing more then they have
+        portfolioService.withdraw(id, value);
     }
 
 }
